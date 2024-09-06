87371378dfe6fed91f981c36e57f47df7ce5dae5
WiNkY51 <110772668+WiNkY51@users.noreply.github.com>
2024-09-06 16:01:19 +0300

    начало создания класса настроек

---
+++ PersonalQuiz/ViewControllers/DevInfoViewController.swift
@@ -0,0 +1,20 @@
+//
+//  DevInfoViewController.swift
+//  PersonalQuiz
+//
+//  Created by Winky51 on 06.09.2024.
+//
+
+import UIKit
+
+class DevInfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+    
+
+
+}
